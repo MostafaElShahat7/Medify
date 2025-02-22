509e4516ed3f6ac5ef97b55c7ee7d43f75384436
asaber3030 <abdulrahmansaber70@gmail.com>
2025-02-23 01:20:29 +0200

    d

--- api/index.js
+++ api/index.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const mongoose = require('mongoose');
-const rateLimit = require('express-rate-limit');
-require('dotenv').config();
-const appointmentRoutes = require("./routes/appointment.routes");
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const rateLimit = require("express-rate-limit");
+require("dotenv").config();
+const appointmentRoutes = require("../routes/appointment.routes");
 
-const { errorHandler } = require('./middleware/errorHandler');
-const { setupLogging } = require('./config/logging');
-const { initializeFirebase } = require('./config/firebase');
-const { createUploadDirs } = require('./config/upload');
-const routes = require('./routes');
-
+const { errorHandler } = require("../middleware/errorHandler");
+const { setupLogging } = require("../config/logging");
+const { initializeFirebase } = require("../config/firebase");
+const { createUploadDirs } = require("../config/upload");
+const routes = require("../routes");
 
 // Initialize Express app
 const app = express();
@@ -39,31 +38,31 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // Serve uploaded files
-app.use('/uploads', express.static(process.env.UPLOAD_PATH));
+app.use("/uploads", express.static(process.env.UPLOAD_PATH));
 
 // Routes
-app.use('/api', routes);
+app.use("/api", routes);
 
 // Error handling
 app.use(errorHandler);
-app.get('/', (req, res) => {
-  res.send('Welcome to the Medify API server!');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Medify API server!");
 });
 
 // Use appointment routes
-app.use("/api", appointmentRoutes); 
-
+app.use("/api", appointmentRoutes);
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
