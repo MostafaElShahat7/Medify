18907927555eb0aebd96a5b71818fd1768e452d2
Mostafa <mostafaelshahat194@gmail.com>
2025-07-11 16:21:14 +0300

    fix some issues related to default values of patient data

--- src/scripts/updatePatients.js
+++ src/scripts/updatePatients.js
@@ -1,38 +1,5 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-
-async function resetDatabase() {
-  try {
-    // Connect to MongoDB
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Connected to MongoDB');
-
-    // Get all collections
-    const collections = await mongoose.connection.db.collections();
-
-    // Drop each collection
-    for (let collection of collections) {
-      await collection.drop();
-      console.log(`Dropped collection: ${collection.collectionName}`);
-    }
-
-    // Drop all indexes
-    const db = mongoose.connection.db;
-    const collections2 = await db.listCollections().toArray();
-    for (let collection of collections2) {
-      await db.collection(collection.name).dropIndexes();
-      console.log(`Dropped indexes for collection: ${collection.name}`);
-    }
-
-    console.log('Database reset complete');
-    process.exit(0);
-  } catch (error) {
-    console.error('Error resetting database:', error);
-    process.exit(1);
-  }
-}
-
-// New script to update all patients with missing required fields
 const Patient = require('../models/patient.model');
 
 async function updatePatientsWithDefaults() {
@@ -82,9 +49,6 @@ async function updatePatientsWithDefaults() {
   }
 }
 
-// Only run if this script is called directly
 if (require.main === module) {
   updatePatientsWithDefaults();
-}
-
-resetDatabase();
+} 
